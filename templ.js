a20c61234cf943c7b9a4301d41d844fc76e04859
tyarchy <ty.d.gregory@gmail.com>
2021-12-31 13:08:32 -0700

    first portfolio draft

---
+++ templ.js
@@ -0,0 +1,13 @@
+// user enters page and is greeted by calc buttons and a answer field
+    // make buttons for each number and an h1 to show text
+        // buttons should feed into a variable when clicked
+// user clicks number buttons, and sees them in the field
+    // add event listeners to the buttons and alter inner html of h1
+        // variable should accept any number of button clicks until operator is chosen
+// user clicks operator button and gets the symbol show in field
+    // add event listener to operator buttons and store the function for use on equals
+        // 
+// user clicked equals button and gets the answer
+    // build a function to return the result of chosen operator
+
+    WHEN i click a button I should see the number
